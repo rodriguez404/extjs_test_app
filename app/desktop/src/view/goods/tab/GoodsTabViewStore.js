fdbdb9ee45f7d4ecdcabea306beb36af06c1f9a6
rodriguez404 <mamaknifys52@yandex.ru>
2025-09-20 23:27:54 +0300

    moved store data to JSON

--- app/desktop/src/view/goods/tab/GoodsTabViewStore.js
+++ app/desktop/src/view/goods/tab/GoodsTabViewStore.js
@@ -8,17 +8,14 @@ Ext.define('extJS_Test_Task.view.home.GoodsViewStore', {
         { name: 'cost',         type: 'number' },
         { name: 'amount',       type: 'number' },
     ],
-    data: { items: [
-        { id: "1", name: "Jean Luc",   description: "jeanluc.picard@enterprise.com", cost: "5168761", amount: "0"},
-        { id: "2", name: "ModernWorf", description: "worf.moghsson@enterprise.com",  cost: "51282", amount: "1515" },
-        { id: "3", name: "Deanna",     description: "deanna.troi@enterprise.com",    cost: "533", amount: "15" },
-        { id: "4", name: "Data",       description: "mr.data@enterprise.com",        cost: "5554444444.1222", amount: "2" }
-    ]},
-    proxy: {
-        type: 'memory',
+	proxy: {
+		type: 'ajax',
         reader: {
             type: 'json',
             rootProperty: 'items'
-        }
-    },    
+        },
+		url: 'resources/desktop/goods.json'
+	},
+	autoLoad: true
+
 });
